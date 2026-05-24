75edd1dde74a2a4ca842460040c2dcdc4741f3f5
Al Pakka <jorim.bechtle@gmx.de>
2026-05-24 18:38:00 +0200

    minor fix

--- custom_components/rainradar/config_flow.py
+++ custom_components/rainradar/config_flow.py
@@ -7,7 +7,6 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
 from homeassistant.core import callback
-from homeassistant.helpers import selector
 
 from .const import (
     DOMAIN,
@@ -17,15 +16,16 @@ from .const import (
     DEFAULT_SCAN_INTERVAL,
 )
 
+MINUTES = 60
+
 
 class RainradarConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input: dict[str, Any] | None = None):
         if user_input is not None:
-            loc_name = user_input.get("name", "Home")
             locations = [{
-                "name": loc_name,
+                "name": user_input.get("name", "Home"),
                 CONF_LATITUDE: user_input[CONF_LATITUDE],
                 CONF_LONGITUDE: user_input[CONF_LONGITUDE],
             }]
@@ -46,32 +46,12 @@ class RainradarConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema(
                 {
                     vol.Required("name", default="Home"): str,
-                    vol.Required(CONF_LATITUDE): selector.NumberSelector(
-                        selector.NumberSelectorConfig(
-                            mode=selector.NumberSelectorMode.BOX,
-                            step=0.000001,
-                            min=-90, max=90,
-                        )
-                    ),
-                    vol.Required(CONF_LONGITUDE): selector.NumberSelector(
-                        selector.NumberSelectorConfig(
-                            mode=selector.NumberSelectorMode.BOX,
-                            step=0.000001,
-                            min=-180, max=180,
-                        )
-                    ),
-                    vol.Optional(CONF_DEVICE_TRACKER): selector.EntitySelector(
-                        selector.EntitySelectorConfig(domain="device_tracker")
-                    ),
+                    vol.Required(CONF_LATITUDE): vol.Coerce(float),
+                    vol.Required(CONF_LONGITUDE): vol.Coerce(float),
+                    vol.Optional(CONF_DEVICE_TRACKER): str,
                     vol.Optional(
                         CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL
-                    ): selector.NumberSelector(
-                        selector.NumberSelectorConfig(
-                            mode=selector.NumberSelectorMode.BOX,
-                            step=10, min=60, max=3600,
-                            unit_of_measurement="seconds",
-                        )
-                    ),
+                    ): vol.All(vol.Coerce(int), vol.Range(min=60, max=3600)),
                 }
             ),
         )
@@ -89,11 +69,17 @@ class RainradarOptionsFlow(config_entries.OptionsFlow):
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input: dict[str, Any] | None = None):
+        current_locations = self.config_entry.options.get(CONF_LOCATIONS, [])
+        device_tracker = self.config_entry.options.get(CONF_DEVICE_TRACKER)
+        scan_interval = self.config_entry.options.get(
+            CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
+        )
+
         if user_input is not None:
             return self.async_create_entry(
                 title="",
                 data={
-                    CONF_LOCATIONS: user_input.get(CONF_LOCATIONS, []),
+                    CONF_LOCATIONS: current_locations,
                     CONF_DEVICE_TRACKER: user_input.get(CONF_DEVICE_TRACKER),
                     CONF_SCAN_INTERVAL: user_input.get(
                         CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
@@ -101,35 +87,15 @@ class RainradarOptionsFlow(config_entries.OptionsFlow):
                 },
             )
 
-        current_locations = self.config_entry.options.get(CONF_LOCATIONS, [])
-        device_tracker = self.config_entry.options.get(CONF_DEVICE_TRACKER)
-        scan_interval = self.config_entry.options.get(
-            CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
-        )
-
-        loc_preview = "; ".join(
-            f'{l.get("name", "?")} ({l.get(CONF_LATITUDE, "?")}, {l.get(CONF_LONGITUDE, "?")})'
-            for l in current_locations
-        ) or "none"
-
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema(
                 {
-                    vol.Optional("locations", default=loc_preview): str,
-                    vol.Optional(CONF_DEVICE_TRACKER, default=device_tracker): selector.EntitySelector(
-                        selector.EntitySelectorConfig(domain="device_tracker")
-                    ),
+                    vol.Optional(CONF_DEVICE_TRACKER, default=device_tracker): str,
                     vol.Optional(
                         CONF_SCAN_INTERVAL,
                         default=scan_interval,
-                    ): selector.NumberSelector(
-                        selector.NumberSelectorConfig(
-                            mode=selector.NumberSelectorMode.BOX,
-                            step=10, min=60, max=3600,
-                            unit_of_measurement="seconds",
-                        )
-                    ),
+                    ): vol.All(vol.Coerce(int), vol.Range(min=60, max=3600)),
                 }
             ),
         )
